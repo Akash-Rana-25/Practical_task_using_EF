29a88f10f308235655ff0e2e2ada8fc35f02543c
Akash-Rana-25 <akashrana.work.gmail.com>
2023-05-16 18:58:24 +0530

    EF task added

---
+++ testDB_using_EF/CrudOpration.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Contexts;
+using ConsoleTables;
+
+namespace testDB_using_EF
+{
+    internal class CrudOpration : ICrudOpration
+    {
+        public void Add()
+        {
+
+            var newEmployee = new Employee()
+            {
+                FirstName = "Pranaya",
+                MiddleName = "J",
+                LastName = "Rout",
+                EmpCode = 12,
+                Gender = 1,
+                DOB = DateTime.Now,
+                salary = 54166,
+                JoiningDate = DateTime.Now,
+                ResignDate = DateTime.Now,
+
+            };
+            using (EmployeeEntities context = new EmployeeEntities())
+            {
+
+                context.Employees.Add(newEmployee);
+                context.SaveChanges();
+
+
+            }
+        }
+        public void Delete(int Id)
+        {
+            using (EmployeeEntities context = new EmployeeEntities())
+            {
+                    var removeEmployee = context.Employees.Find(Id);
+
+                if (removeEmployee != null)
+                {
+                    context.Employees.Remove(removeEmployee);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    Console.WriteLine("\n Employee Does Not Exist");
+
+                }
+            }
+
+        }
+        public void Update(int Id)
+        {
+            using (EmployeeEntities context = new EmployeeEntities())
+            {
+                var Emp = context.Employees.Find(Id);
+
+                if (Emp != null)
+                {
+                    Emp.FirstName = "Sanju";
+                    Emp.LastName = "Samson";
+
+
+                    context.SaveChanges();
+                }
+                else
+                {
+                    Console.WriteLine("\n Employee Does Not Exist");
+                }
+
+            }
+        }
+        public void Read()
+        {
+
+            using (EmployeeEntities context = new EmployeeEntities())
+            {
+
+
+                var AllEmployees = context.Employees.ToList();
+                var table = new ConsoleTable("Id", "First Name", "Middle Name", "Last Name", "Employee Code", "DOB", "Salary","Gender", "Joining Date", "Resign Date");
+
+
+                foreach (var Emp in AllEmployees)
+                {
+                    table.AddRow(Emp.Employee_PK,Emp.FirstName,Emp.MiddleName,Emp.LastName,Emp.EmpCode,Emp.DOB,Emp.salary,Emp.Gender,Emp.JoiningDate,Emp.ResignDate);
+                }
+                table.Options.EnableCount = false;
+                table.Write();
+            }
+
+        }
+        public void Read(int Id)
+        {
+           
+            using (EmployeeEntities context = new EmployeeEntities())
+            {
+                var Emp = context.Employees.Find(Id);
+
+                if (Emp != null)
+                {
+                    var table = new ConsoleTable("Id", "First Name", "Middle Name", "Last Name", "Employee Code", "DOB", "Salary", "Gender", "Joining Date", "Resign Date");
+
+                    table.AddRow(Emp.Employee_PK, Emp.FirstName, Emp.MiddleName, Emp.LastName, Emp.EmpCode, Emp.DOB, Emp.salary, Emp.Gender, Emp.JoiningDate, Emp.ResignDate);
+                    table.Options.EnableCount = false;
+                    table.Write();
+                }
+                else {
+                    Console.WriteLine("\n Employee Does Not Exist");
+
+                }
+
+
+
+
+
+            }
+        }
+
+    }
+}
---
+++ testDB_using_EF/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testDB_using_EF
+{
+    internal class Program
+    {
+        public enum crud
+        {
+            Getall = 0,
+            insert = 1,
+            update = 2,
+            delete = 3,
+            GetByID = 4
+        }
+        static void Main(string[] args)
+        {
+            string status;
+
+            do
+            {
+                Console.WriteLine($"\n Enter Your Choice:");
+                Console.WriteLine($"0 - Get ALL");
+                Console.WriteLine($"1 - Insert");
+                Console.WriteLine($"2 - Update");
+                Console.WriteLine($"3 - Delete");
+                Console.WriteLine($"4 - Get BY ID \n");
+
+
+
+                string choice = Console.ReadLine();
+                int intChoice = Convert.ToInt32(choice);
+                crud userSelected = (crud)intChoice;
+
+                switch (userSelected)
+                {
+                    case crud.insert:
+
+                        CrudOpration insert = new CrudOpration();
+                        insert.Add();
+
+                        break;
+
+                    case crud.update:
+                        Console.WriteLine("Enter Id: ");
+                        int UpdateId;
+
+                        if (int.TryParse(Console.ReadLine(), out UpdateId))
+                        {
+                            CrudOpration update = new CrudOpration();
+                            update.Update(UpdateId);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Pelese Provide Valid Input");
+
+                        }
+                        break;
+
+                    case crud.delete:
+                        Console.WriteLine("Enter Id: ");
+                        int Id;
+                        if (int.TryParse(Console.ReadLine(), out Id))
+                        {
+                            CrudOpration remove = new CrudOpration();
+                            remove.Delete(Id);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Pelese Provide Valid Input");
+
+                        }
+
+                        break;
+
+                    case crud.Getall:
+                        CrudOpration read = new CrudOpration();
+                        read.Read();
+
+                        break;
+
+
+
+                    case crud.GetByID:
+                        Console.WriteLine("Enter Id : ");
+                        int readId;
+                        if (int.TryParse(Console.ReadLine(), out readId))
+                        {
+                            CrudOpration readByID = new CrudOpration();
+                            readByID.Read(readId);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Pelese Provide Valid Input");
+
+                        }
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Pelese Provide Valid Input");
+                        break;
+                }
+
+                Console.ReadLine();
+                Console.Write("Do you want to continue(y/n):");
+                status = Console.ReadLine();
+            }
+            while (status == "y" || status == "Y");
+           
+
+         
+           
+        
+
+
+            Console.ReadKey();
+        }
+    }
+}
